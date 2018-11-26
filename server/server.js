96141b8e69a85a1d0b75d7dfb602f6223c6f90b8
Unknown <toutlemonde74@gmail.com>
2018-11-26 13:02:58 +0100

    syncro all 3

--- server/server.js
+++ server/server.js
@@ -6,17 +6,14 @@ var app = express();
 var server = http.Server(app);
 var io = socketio(server);
 
+var activeClients =[]
+var currentIndex = 1
+currentScreen = 5
 var pas = 1
-var activeClients = []
-var currentClient = 0
-var i = 0;
-var vasVersDroit = true;
-var currentScreen = 1;
 io.on("connection", function(socket) {
 	console.log("Connect!");
 	socket.emit("giveRole", i)
 	
-	if(i!=0)
 		activeClients.push(socket)
 
 	i++
@@ -27,20 +24,8 @@ io.on("connection", function(socket) {
 
 
 setInterval(function(){
-	currentScreen = activeClients[currentClient]
-	currentClient+=pas
-	if(currentClient==0)
-	{
-		vasVersDroit = true
-		pas =-pas
-	}
-	if(currentClient == activeClients.length)
-	{
-		vasVersDroit = false
-		pas =-pas
-	}
-	console.log({"active" : currentScreen,"vasVersDroit" : vasVersDroit})
-	currentScreen.emit("ChangeScreen", {"active" : currentScreen,"vasVersDroit" : vasVersDroit})
+	
+	activeClients[currentIndex].emit("ChangeScreen", {"active" : currentScreen,"vasVersDroit" : vasVersDroit})
 },2000)
 
 
