271a033de07f6d8dadd927a3b9d5ec8810123aed
Unknown <toutlemonde74@gmail.com>
2018-11-26 12:39:41 +0100

    serveur multi co

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ var app = express();
 var server = http.Server(app);
 var io = socketio(server);
 
-
+var activeClients = []
+var currentClient = 0
 var i = 0;
 var vasVersDroit = true;
 var currentScreen = 0;
@@ -14,24 +15,20 @@ io.on("connection", function(socket) {
 	console.log("Connect!");
 	socket.emit("giveRole", i)
 	i++
-
-	socket.emit("ChangeScreen", {"active" : currentScreen,"vasVersDroit" : vasVersDroit})
-
-	socket.on("boum", function(data) {
-		if (data.author != "")
-			io.emit("boum",data)
-	})
-	socket.on("move", function(data) {
-		if (data.author != "")
-			io.emit("move",data)
-	})
-	socket.on("point", function(data) {
-		if (data.author != "")
-			io.emit("point",data)
-	})
+	activeClients.push(i)
+	
 
 
 })
+setInterval(function(){
+	currentScreen = activeClients[currentClient]
+	if(currentClient==0)
+		vasVersDroit = true
+	if(currentClient == activeClients.length)
+		vasVersDroit = false
+	socket.emit("ChangeScreen", {"active" : currentScreen,"vasVersDroit" : vasVersDroit})
+},2000)
+
 
 server.listen(8083);
 
